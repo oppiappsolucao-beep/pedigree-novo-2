1468fb4b84bbf8299bb8da7926f5d9d62d07b103
oppiappsolucao-beep <oppiappsolucao@gmail.com>
2026-04-22 16:41:14 -0300

    Update title in app.py to 'OK'

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
 import streamlit as st
 
-st.set_page_config(page_title="Teste", layout="wide")
-st.title("Teste puro")
-st.write("Se isso abriu, o problema não é o Streamlit em si.")
+st.title("OK")
