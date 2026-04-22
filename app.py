e153acebcd5cc61f06e2a013ebc24796a3d54a5d
oppiappsolucao-beep <oppiappsolucao@gmail.com>
2026-04-22 16:38:36 -0300

    Update title and content in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.set_page_config(page_title="Teste", layout="wide")
-st.title("App funcionando")
-st.write("Deploy ok")
+st.title("Teste puro")
+st.write("Se isso abriu, o problema não é o Streamlit em si.")
